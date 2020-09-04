05c972ba6c39921ed1e6e0a59cc2e210cc71c160
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-09-04 19:12:38 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React, { Component } from "react";
-import Menu from "./components/Menu/Menu";
-import Data from "./data.json";
-import Grid from "./components/Grid/Grid";
+import Grid from "./data/dataGrid.json";
+import MenuPost from "./data/dataMenuPost.json";
+import MenuNav from "./data/dataMenuNav.json";
+import Stories from "./data/dataStories.json";
 import Aux from "./hoc/Aux";
-
-// import Motor from "./imgInSrc/Motor/Motor";
-// import { render } from "node-sass";
+import HeaderElements from "./components/Header/HeaderElements";
+import NavigationElements from "./components/Navigation/NavigationElements";
+import PostGridElements from "./components/PostGrid/PostGridElements";
+import StoriesElements from "./components/Stories/StoriesElements";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // userInput: "",
-      // userName: "Hadi",
-      image: "",
-    };
-
-    this.change = (e) => {
-      e.preventDefault();
-      this.setSate({
-        // userName: "Ali",
-        imageNew: "",
-      });
+      dataGrid: Grid,
+      dataMenuPost: MenuPost,
+      dataMenuNav: MenuNav,
+      dataStories: Stories,
+      filteredData: [],
     };
   }
 
   render() {
+    console.log(this.state.data, "filtered", this.state.filteredData);
+    console.log(this.state.dataMenu);
+
     return (
       <Aux>
-        <Menu propsName="instagram" />
-        <Grid info={Data} />
+        <NavigationElements />
+        <HeaderElements />
+        <PostGridElements />
+        <StoriesElements />
       </Aux>
     );
   }
