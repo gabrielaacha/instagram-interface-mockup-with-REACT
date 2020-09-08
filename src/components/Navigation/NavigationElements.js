71bb036bd81ccc09cc88b6527f4712a50708a700
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-09-08 18:01:21 +0200

    nav styling

--- src/components/Navigation/NavigationElements.js
+++ src/components/Navigation/NavigationElements.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import NavigationElementLogo from "./NavigationElement/NavigationElementLogo";
+import NavigationElementSearch from "./NavigationElement/NavigationElementSearch";
+import NavigationElementIcons from "./NavigationElement/NavigationElementIcons";
 
 const navigationElements = (props) => {
   return (
     <Aux>
-      <div className="navigation-elements__all">navigationelements</div>
+      <div className="navigation-elements__all">
+        <NavigationElementLogo />
+        <NavigationElementSearch />
+        <NavigationElementIcons />
+      </div>
     </Aux>
   );
 };
