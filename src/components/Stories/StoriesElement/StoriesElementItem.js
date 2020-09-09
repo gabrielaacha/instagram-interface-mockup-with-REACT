bd628b9f0df4581b252c1c8d6cc86de5a4b96b09
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-09-09 09:35:13 +0200

    stories styling

--- src/components/Stories/StoriesElement/StoriesElementItem.js
+++ src/components/Stories/StoriesElement/StoriesElementItem.js
@@ -4,7 +4,7 @@ const storiesElementItem = (props) => {
   const { name, url } = props.info;
   return (
     <div className="stories-elements__item">
-      <img alt={name} src={url} />
+      <img alt={name} src={url} className="stories-elements__img" />
     </div>
   );
 };
