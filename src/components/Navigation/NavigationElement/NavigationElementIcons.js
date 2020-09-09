d2fa3e4090c7d14398be5b8b81f8a1c5ecb9ae82
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-09-09 12:53:53 +0200

    icons styling

--- src/components/Navigation/NavigationElement/NavigationElementIcons.js
+++ src/components/Navigation/NavigationElement/NavigationElementIcons.js
@@ -1,17 +1,34 @@
 import React from "react";
 import Aux from "../../../hoc/Aux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBell,
+  faPaperPlane,
+  faHome,
+  faCompass,
+  faHeart,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 const navigationElementIcons = (props) => {
   return (
     <Aux>
       <div className="navigation-elements__icons">
         {" "}
-        <FontAwesomeIcon className="navigation-elements__icon" icon={faBell} />
-        <FontAwesomeIcon className="navigation-elements__icon" icon={faBell} />
-        <FontAwesomeIcon className="navigation-elements__icon" icon={faBell} />
-        <FontAwesomeIcon className="navigation-elements__icon" icon={faBell} />
+        <FontAwesomeIcon className="navigation-elements__icon" icon={faHome} />
+        <FontAwesomeIcon
+          className="navigation-elements__icon"
+          icon={faPaperPlane}
+        />
+        <FontAwesomeIcon
+          className="navigation-elements__icon"
+          icon={faCompass}
+        />
+        <FontAwesomeIcon className="navigation-elements__icon" icon={faHeart} />
+        <FontAwesomeIcon
+          className="navigation-elements__icon"
+          icon={faUserCircle}
+        />
       </div>
     </Aux>
   );
