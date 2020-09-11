9269cfb55a46a542197b4c0a9fb9f061f105c28a
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-09-11 23:26:07 +0200

    failed attempt

--- src/components/PostMenu/PostMenuElements.js
+++ src/components/PostMenu/PostMenuElements.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Aux from "../../hoc/Aux";
 import PostMenuElementCategory from "./PostMenuElement/PostMenuElementCategory";
 
-const postMenuElements = (props) => {
+export default function PostMenuElements(props) {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   return (
     <Aux>
       <div className="post-menu-elements">
-        <PostMenuElementCategory />
+        <PostMenuElementCategory
+          className="isActive, active"
+          onClick={toggleClass}
+        />
         <PostMenuElementCategory />
         <PostMenuElementCategory />
         <PostMenuElementCategory />
       </div>
     </Aux>
   );
-};
-
-export default postMenuElements;
+}
