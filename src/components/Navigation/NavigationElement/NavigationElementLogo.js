ef9da39713d1935b908601d563d6e3c634d7bafb
Gabriela Acha <gabriela.gabriela.acha.acha@gmail.com>
2020-10-15 22:51:02 +0200

    images

--- src/components/Navigation/NavigationElement/NavigationElementLogo.js
+++ src/components/Navigation/NavigationElement/NavigationElementLogo.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const navigationElementLogo = (props) => {
+const navigationElementLogo = () => {
+  let img1 = "./img/instagramlogo.png";
+  let img2 = "./img/a.jpg";
   return (
     <div className="navigation-elements__logo">
-      <img src="./instagram_logo.png" alt="insta logo" />
+      <img src={img1} />
+      <p>Hello</p>
     </div>
   );
 };
